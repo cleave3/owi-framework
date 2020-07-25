3601e2c032e55f7d3360448ebb68a16deebb0252
cleave3 <owhiroroeghele@gmail.com>
2020-07-25 10:07:41 +0100

    set up php documentor

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-
-use Symfony\Component\Dotenv\Dotenv;
-
-$dotenv = new Dotenv();
-$dotenv->load(__DIR__ . '/.env');
-
-var_dump($_ENV);
--- test.php
+++ test.php
@@ -1,5 +1,17 @@
 <?php
-putenv("TEST=JJJ");
-$_ENV["TEST"] = "barney";
-echo getenv("TEST");
-echo $_ENV["TEST"];
+
+// use Html\Table;
+// putenv("TEST=JJJ");
+// $_ENV["TEST"] = "barney";
+// echo getenv("TEST");
+// echo $_ENV["TEST"];
+// class Fruit
+// {
+//     public $name;
+// }
+
+// $apple = new Fruit();
+// echo json_encode($apple instanceof Fruit);
+$table = new Html\Table();
+
+echo $table->message();
